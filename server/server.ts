af003523853b1331d1dab112cace18d2719547a0
james-bellion <belliondjames@hotmail.co.uk>
2023-07-13 05:57:17 +1200

    tested backend route through insomnia, all good

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import path from 'path'
 
+import items from './routes/items'
+
 const server = express()
 
 server.use(express.json())
+
+
+server.use('/api/v1/items', items)
+
+
 server.use(express.static(path.join(__dirname, 'public')))
 
 export default server
