a14f0fa26912846f20d8d8c87480188e8a492d79
james-bellion <belliondjames@hotmail.co.uk>
2023-07-18 22:26:50 +1200

    added font may need to do small fix on btn back to homepage, going to bed now

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,31 +1,11 @@
-import List from './List'
-import AddItem from './AddItem'
-import { useAppDispatch } from '../hooks/hooks'
-import { useEffect } from 'react'
-import * as itemsActions from "../actions/items" 
-
 import { Outlet } from 'react-router-dom'
 
 
 function App() {
 
-  
-
-  // useEffect(() => {
-  //   dispatch(itemsActions.getItemsThunk())
-  // }, [dispatch])
-
-
   return (
     <>
-      {/* <header className="header">
-        <h1>My Collection</h1>
-      </header>
-      <AddItem />
-      <List /> */}
-
       <Outlet />
-     
     </>
   )
 }
--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -9,7 +9,7 @@ const navigate = useNavigate()
         <>
         <div className='homepage-center-container'>
             <img className="homepage-logo" src="Public/images/List-It-logos_transparent.png" alt="homepage-logo"/>
-            <button className='home-page-btn click-btn btn-style701' onClick={() => navigate('/list')}>My List</button>
+            <button className='home-page-btn click-btn btn-style701' onClick={() => navigate('/list')} style={{ cursor: 'pointer' }}>My List</button>
 
         </div>
         </>
