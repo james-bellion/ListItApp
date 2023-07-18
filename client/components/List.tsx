9eedb5ec23dff6f258503f4a0a386ed82ac4c365
james-bellion <belliondjames@hotmail.co.uk>
2023-07-18 21:15:09 +1200

    sorted the update and checkbox

--- client/components/List.tsx
+++ client/components/List.tsx
@@ -1,198 +1,4 @@
-// import { useAppDispatch, useAppSelector } from '../hooks/hooks';
-// import { toggleItem, getItemsThunk, updateItemThunk } from '../actions/items';
-// import { RootState } from '../store';
-// import { Item, ItemData } from '../../models/types';
-// import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-// import * as action from '../actions/items';
-// import { useNavigate } from 'react-router-dom'
-// import AddItem from './AddItem';
-
-// function List() {
-//   const [formData, setFormData] = useState<string>('');
-//   const [showForm, setShowForm] = useState(false);
-//   const dispatch = useAppDispatch();
-//   const itemsArr = useAppSelector((state: RootState) => state.items) as Item[];
-
-//   const navigate = useNavigate()
-
-//   // Handle checkbox change with a default value for 'completed' to avoid uncontrolled input warning
-//   const handleCheckboxChange = (id: number, completed: boolean | undefined) => {
-//     // Toggle the completed status by using !completed if it's defined, otherwise default to false
-//     dispatch(toggleItem(id, completed !== undefined ? !completed : false));
-//   };
-
-//   const handleDelete = (id: number) => {
-//     dispatch(action.deleteItemThunk(id));
-//   };
-
-//   const handleSubmit = (evt: FormEvent, id: number, formData: string) => {
-//     evt.preventDefault();
-//     console.log('submit:', formData);
-//     dispatch(action.updateListItemThunk(id, formData));
-//   };
-
-//   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
-//     const { value } = evt.target;
-//     setFormData(value);
-//   };
-
-//   useEffect(() => {
-//     dispatch(getItemsThunk());
-//   }, [dispatch]);
-
-//   return (
-//     <div className='list-container'>
-//       <div className='notebook'>
-
-//       <h1 className='title'>My List</h1>
-//       <AddItem />
-//       <ul>
-//         {itemsArr.map((item) => (
-//           <li key={item.id}>
-//             <label>
-//               <input
-//                 className='input-container'
-//                 type="checkbox"
-//                 checked={item.completed !== undefined ? item.completed : false} // Add default value for 'completed'
-//                 onChange={() => handleCheckboxChange(item.id, item.completed)}
-//               />
-//               <span
-//                 style={{
-//                   textDecoration: item.completed ? 'line-through' : 'none',
-//                 }}
-//               >
-//                 {item.item}
-//               </span>
-//             </label>
-//             <button onClick={() => handleDelete(item.id)}>Delete</button>
-
-//             <button className="update-btn" onClick={() => setShowForm(!showForm)}>
-//               Edit
-//             </button>
-//             {showForm && (
-//               <form onSubmit={(evt) => handleSubmit(evt, item.id, formData)}>
-//                 <label htmlFor="rating">Edit</label>
-//                 <input
-//                   type="text"
-//                   id="new-item"
-//                   name="new-item"
-//                   value={formData}
-//                   onChange={handleChange}
-//                 />
-//                 <input type="submit" value="Update" />
-//               </form>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-
-//       <button onClick={() => navigate('/')}>home</button>
-//       </div>
-      
-//     </div>
-//   );
-// }
-
-// export default List;
-
-
-
-// nearly there!!!!!!!
-// import { useState, ChangeEvent, FormEvent, useEffect } from 'react';
-// import { useAppDispatch, useAppSelector } from '../hooks/hooks';
-// import { RootState } from '../store';
-// import { Item } from '../../models/types';
-// import { toggleItem, getItemsThunk, updateListItemThunk, deleteItemThunk } from '../actions/items';
-// import { useNavigate } from 'react-router-dom';
-// import AddItem from './AddItem';
-
-// function List() {
-//   const [formData, setFormData] = useState<string>('');
-//   const [editingItemId, setEditingItemId] = useState<number | null>(null);
-//   const dispatch = useAppDispatch();
-//   const itemsArr = useAppSelector((state: RootState) => state.items) as Item[];
-
-//   const navigate = useNavigate();
-
-//   const handleCheckboxChange = (id: number, completed: boolean | undefined) => {
-//     dispatch(toggleItem(id, completed !== undefined ? !completed : false));
-//   };
-
-//   const handleDelete = (id: number) => {
-//     dispatch(deleteItemThunk(id));
-//   };
-
-//   const handleItemClick = (id: number) => {
-//     setEditingItemId(id);
-//   };
-
-//   const handleSubmit = (evt: FormEvent, id: number, formData: string) => {
-//     evt.preventDefault();
-//     console.log('submit:', formData);
-//     dispatch(updateListItemThunk(id, formData));
-//     setEditingItemId(null);
-//   };
-
-//   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
-//     const { value } = evt.target;
-//     setFormData(value);
-//   };
-
-//   useEffect(() => {
-//     dispatch(getItemsThunk());
-//   }, [dispatch]);
-
-//   return (
-//     <div className="list-container">
-//       <div className="notebook">
-//         <h1 className="title">My List</h1>
-//         <AddItem />
-//         <ul>
-//           {itemsArr.map((item) => (
-//             <li key={item.id} onClick={() => handleItemClick(item.id)}>
-//               {editingItemId === item.id ? (
-//                 <form onSubmit={(evt) => handleSubmit(evt, item.id, formData)}>
-//                   <input
-//                     type="text"
-//                     id="new-item"
-//                     name="new-item"
-//                     value={formData}
-//                     onChange={handleChange}
-//                     onKeyPress={(evt) => {
-//                       if (evt.key === 'Enter') {
-//                         handleSubmit(evt, item.id, formData);
-//                       }
-//                     }}
-//                   />
-//                   <input type="submit" value="Update" />
-//                 </form>
-//               ) : (
-//                 <label>
-//                   <input
-//                     className="input-container"
-//                     type="checkbox"
-//                     checked={item.completed !== undefined ? item.completed : false}
-//                     onChange={() => handleCheckboxChange(item.id, item.completed)}
-//                   />
-//                   <span style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>
-//                     {item.item}
-//                   </span>
-//                 </label>
-//               )}
-//               <button onClick={() => handleDelete(item.id)}>Delete</button>
-//             </li>
-//           ))}
-//         </ul>
-//         <button onClick={() => navigate('/')}>home</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default List;
-
-
-// working good just need to change the layout 
+// here goes
 import { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../hooks/hooks';
 import { RootState } from '../store';
@@ -204,12 +10,16 @@ import AddItem from './AddItem';
 function List() {
   const [formData, setFormData] = useState<string>('');
   const [editingItemId, setEditingItemId] = useState<number | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [clickStartTime, setClickStartTime] = useState(0);
   const dispatch = useAppDispatch();
   const itemsArr = useAppSelector((state: RootState) => state.items) as Item[];
 
   const navigate = useNavigate();
 
-  const handleCheckboxChange = (id: number, completed: boolean | undefined) => {
+  const handleCheckboxChange = (evt: ChangeEvent<HTMLInputElement>, id: number, completed: boolean | undefined) => {
+    // Prevent the event from bubbling up and triggering the li click event
+    evt.stopPropagation();
     dispatch(toggleItem(id, completed !== undefined ? !completed : false));
   };
 
@@ -217,14 +27,24 @@ function List() {
     dispatch(deleteItemThunk(id));
   };
 
-  const handleItemClick = (id: number) => {
-    setEditingItemId(id);
+  const handleItemMouseDown = (id: number) => {
+    setClickStartTime(Date.now());
+  };
+
+  const handleItemMouseUp = (id: number) => {
+    const clickDuration = Date.now() - clickStartTime;
+    // If the click duration is greater than 2 seconds, open the edit form
+    if (clickDuration >= 1000) {
+      setEditingItemId(id);
+      setIsEditing(true);
+    }
   };
 
   const handleSubmit = (id: number, formData: string) => {
     console.log('submit:', formData);
     dispatch(updateListItemThunk(id, formData));
     setEditingItemId(null);
+    setIsEditing(false); // Close the edit form after submission
   };
 
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
@@ -243,41 +63,49 @@ function List() {
         <AddItem />
         <ul style={{ listStyleType: 'none' }}>
           {itemsArr.map((item) => (
-            <li key={item.id} onClick={() => handleItemClick(item.id)}>
-              {editingItemId === item.id ? (
-                <form
-                  onSubmit={(evt) => {
-                    evt.preventDefault();
-                    handleSubmit(item.id, formData);
-                  }}
-                >
-                  <input
-                    type="text"
-                    id="new-item"
-                    name="new-item"
-                    value={formData}
-                    onChange={handleChange}
-                    onKeyPress={(evt) => {
-                      if (evt.key === 'Enter') {
-                        handleSubmit(item.id, formData);
-                      }
+            <li
+              key={item.id}
+              onMouseDown={() => handleItemMouseDown(item.id)}
+              onMouseUp={() => handleItemMouseUp(item.id)}
+              className="list-item"
+            >
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  checked={item.completed !== undefined ? item.completed : false}
+                  onChange={(evt) => handleCheckboxChange(evt, item.id, item.completed)}
+                />
+              </div>
+              <div className="item-content">
+                {editingItemId === item.id && isEditing ? (
+                  <form
+                    onSubmit={(evt) => {
+                      evt.preventDefault();
+                      handleSubmit(item.id, formData);
                     }}
-                  />
-                </form>
-              ) : (
-                <label>
-                  <input
-                    className="input-container"
-                    type="checkbox"
-                    checked={item.completed !== undefined ? item.completed : false}
-                    onChange={() => handleCheckboxChange(item.id, item.completed)}
-                  />
-                  <span style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>
-                    {item.item}
-                  </span>
-                </label>
-              )}
-              <button onClick={() => handleDelete(item.id)}>Delete</button>
+                  >
+                    <input
+                      type="text"
+                      id="new-item"
+                      name="new-item"
+                      value={formData}
+                      onChange={handleChange}
+                      onKeyPress={(evt) => {
+                        if (evt.key === 'Enter') {
+                          handleSubmit(item.id, formData);
+                        }
+                      }}
+                    />
+                  </form>
+                ) : (
+                  <label>
+                    <span style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>
+                      {item.item}
+                    </span>
+                  </label>
+                )}
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
+              </div>
             </li>
           ))}
         </ul>
@@ -288,7 +116,6 @@ function List() {
 }
 
 export default List;
-
 
 
 
